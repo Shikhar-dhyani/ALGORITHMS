bb485736b0b772fe4ff2ff86a196e6534ed93ee4
Shikhar Dhyani <shikhardhyani025@gmail.com>
2022-01-17 20:08:09 +0530

    Add files via upload

---
+++ JUMP_SEARCH.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+void JUMPSEARCH(int arr[], int x, int n)
+{
+    int block = sqrt(n);
+    int pre = 0;
+    while (arr[min(block, n)-1] < x)
+    {
+        pre = block;
+        block += sqrt(n);
+        if (pre >= n)
+        {
+            cout << "Element not found 1" << endl;
+            return;
+        }
+    }
+    while (arr[pre] < x)
+    {
+        pre++;
+        if (pre == min(block, n))
+        {
+            cout << "Element not found 2" << endl;
+            return;
+        }
+    }
+    if (arr[pre] == x)
+    {
+        cout << "ELEMENT IS AT " << pre << " index" << endl;
+        return;
+    }
+    else
+    {
+        cout << "Element not found 3" << endl;
+        return;
+    }
+}
+int main(){
+//    int arr[]={1,2,3,5,58,78,98,101};
+//    int n=sizeof(arr)/sizeof(arr[0]);
+//    int x=58;
+   int n,x;
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   cin >> arr[i];
+   cin>> x;
+    JUMPSEARCH(arr,x,n);
+}
